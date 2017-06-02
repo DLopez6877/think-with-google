7d67e6e7d537ee0619e90462d9a97d23b48e11fe
daniel lopez <daniel@>
2017-06-02 14:26:06 -0700

    fix indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 ////////////////////////    USER INTERFACE    ////////////////////////
 $(function(){
+  // add glow to hover
   $(".card").hover(
     function(){ $(this).addClass('glow') },
     function(){ $(this).removeClass('glow') }
@@ -9,14 +10,16 @@ $(function(){
     function(){ $(this).removeClass('glow') }
   )
 
+  // make words change every x seconds
   $(function () {
-  count = 0;
-  wordsArray = ["adore", "admire", "adore", "honor", "adore", "praise"];
-  setInterval(function () {
-    count++;
-    $("#changeText").fadeOut(400, function () {
-      $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
-    });
-  }, 5000);
+    count = 0;
+    wordsArray = ["adore", "admire", "adore", "honor", "adore", "praise"];
+    setInterval(function () {
+      count++;
+      $("#changeText").fadeOut(400, function () {
+        $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
+      });
+    }, 5000);
   });
+  
 });
